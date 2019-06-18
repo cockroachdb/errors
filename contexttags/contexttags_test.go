8e3409f607a20a016b0497899457627a8d203b6e
Raphael 'kena' Poss <knz@cockroachlabs.com>
2019-06-18 09:17:53 +0200

    New API "WithContext" to store logtags buffers.
    This enables capturing the current context (and its tags)
    in an error payload.

---
+++ contexttags/contexttags_test.go
@@ -0,0 +1,85 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package contexttags_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cockroachdb/errors"
+	"github.com/cockroachdb/errors/contexttags"
+	"github.com/cockroachdb/errors/errbase"
+	"github.com/cockroachdb/errors/markers"
+	"github.com/cockroachdb/errors/testutils"
+	"github.com/cockroachdb/logtags"
+)
+
+func TestWithContext(t *testing.T) {
+	tt := testutils.T{T: t}
+
+	// Create an example context with decoration.
+	ctx := context.Background()
+	ctx = logtags.AddTag(ctx, "foo", 123)
+	ctx = logtags.AddTag(ctx, "x", 456)
+	ctx = logtags.AddTag(ctx, "bar", nil)
+
+	// Create another example context. We use two to demonstrate that an
+	// error can store multiple sets of context tags.
+	ctx2 := context.Background()
+	ctx2 = logtags.AddTag(ctx2, "planet", "universe")
+
+	// This will be our reference expected value.
+	refTagsets := []*logtags.Buffer{
+		logtags.SingleTagBuffer("planet", "universe"),
+		logtags.SingleTagBuffer("foo", "123").Add("x", "456").Add("bar", ""),
+	}
+
+	// Construct the error object.
+	origErr := errors.New("hello")
+	decoratedErr := errors.WithContextTags(origErr, ctx)
+	decoratedErr = errors.WithContextTags(decoratedErr, ctx2)
+
+	theTest := func(tt testutils.T, err error) {
+		// Ensure that the original error object can be found.
+		// This test that the cause interface works properly.
+		tt.Check(markers.Is(err, origErr))
+
+		// Ensure that the decorated error can be found.
+		// This checks that the wrapper identity
+		// is properly preserved across the network.
+		tt.Check(markers.Is(err, decoratedErr))
+
+		// Check that the message is preserved.
+		tt.CheckEqual(err.Error(), "hello")
+
+		// Check that the tag pairs are preserved.
+		tagsets := contexttags.GetContextTags(err)
+		if len(tagsets) != len(refTagsets) {
+			tt.CheckEqual(len(tagsets), len(refTagsets))
+		} else {
+			for i, actualB := range tagsets {
+				refB := refTagsets[i]
+				tt.CheckDeepEqual(actualB.Get(), refB.Get())
+			}
+		}
+	}
+
+	tt.Run("local", func(tt testutils.T) { theTest(tt, decoratedErr) })
+
+	enc := errbase.EncodeError(decoratedErr)
+	newErr := errbase.DecodeError(enc)
+
+	tt.Run("remote", func(tt testutils.T) { theTest(tt, newErr) })
+}
---
+++ contexttags/with_context.go
@@ -0,0 +1,96 @@
+// Copyright 2019 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package contexttags
+
+import (
+	"fmt"
+
+	"github.com/cockroachdb/errors/errbase"
+	"github.com/cockroachdb/errors/errorspb"
+	"github.com/cockroachdb/logtags"
+	"github.com/gogo/protobuf/proto"
+)
+
+type withContext struct {
+	cause error
+	// tags stores the context k/v pairs.
+	// The errors library only gives access to the string representation
+	// of the value part. This is because the network encoding of
+	// a withContext instance only stores the string.
+	tags *logtags.Buffer
+}
+
+var _ error = &withContext{}
+
+// withContext is an error. The original error message is preserved.
+func (w *withContext) Error() string { return w.cause.Error() }
+
+// the cause is reachable.
+func (w *withContext) Cause() error  { return w.cause }
+func (w *withContext) Unwrap() error { return w.cause }
+
+// Printing a withContext reveals the tags.
+func (w *withContext) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			fmt.Fprintf(s, "%+v", w.cause)
+			if w.tags != nil {
+				fmt.Fprintf(s, "\n-- context: %s", w.tags.String())
+			}
+			return
+		}
+		fallthrough
+	case 's', 'q':
+		errbase.FormatError(s, verb, w.cause)
+	}
+}
+
+func encodeWithContext(err error) (string, []string, proto.Message) {
+	w := err.(*withContext)
+	p := &errorspb.TagsPayload{}
+	for _, t := range w.tags.Get() {
+		p.Tags = append(p.Tags, errorspb.TagPayload{Tag: t.Key(), Value: t.ValueStr()})
+	}
+	return "", nil, p
+}
+
+func decodeWithContext(cause error, _ string, _ []string, payload proto.Message) error {
+	m, ok := payload.(*errorspb.TagsPayload)
+	if !ok {
+		// If this ever happens, this means some version of the library
+		// (presumably future) changed the payload type, and we're
+		// receiving this here. In this case, give up and let
+		// DecodeError use the opaque type.
+		return nil
+	}
+	if len(m.Tags) == 0 {
+		// There are no tags stored. Either there are no tags stored, or
+		// we received some new version of the protobuf message which does
+		// things differently. Again, use the opaque type.
+		return nil
+	}
+	// Convert the k/v pairs.
+	var b *logtags.Buffer
+	for _, t := range m.Tags {
+		b = b.Add(t.Tag, t.Value)
+	}
+	return &withContext{cause: cause, tags: b}
+}
+
+func init() {
+	errbase.RegisterWrapperEncoder(errbase.GetTypeKey(&withContext{}), encodeWithContext)
+	errbase.RegisterWrapperDecoder(errbase.GetTypeKey(&withContext{}), decodeWithContext)
+}
