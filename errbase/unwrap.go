8a8366e618091b571dd12bf4d7bc339645bdb8eb
David Hartunian <davidh@cockroachlabs.com>
2023-07-27 15:27:37 -0400

    support multi-cause errors in go 1.20
    Go 1.20 introduces the idea of an error with multiple causes instead
    of a single chain. This commit updates the errors library to properly
    encode, decode, and format these error types.
    
    For encoding and decoding we use the existing `EncodedLeaf` type and
    embellish it with a `causes` field. This is done in order to keep the
    encoding/decoding backwards compatible. `EncodedLeaf` types containing
    multiple causes when decided by earlier versions will simply see an
    opaque leaf with a message inside. The reason the `EncodedWrapper`
    is not used here is because the wrapper already contains a mandatory
    single `cause` field that we cannot fill with the multi-errors. A
    new type cannot be used because it would not be decodable by older
    versions of this library.

--- errbase/unwrap.go
+++ errbase/unwrap.go
@@ -27,6 +27,16 @@ package errbase
 // It supports both errors implementing causer (`Cause()` method, from
 // github.com/pkg/errors) and `Wrapper` (`Unwrap()` method, from the
 // Go 2 error proposal).
+//
+// UnwrapOnce treats multi-errors (those implementing the
+// `Unwrap() []error` interface as leaf-nodes since they cannot
+// reasonably be iterated through to a single cause. These errors
+// are typically constructed as a result of `fmt.Errorf` which results
+// in a `wrapErrors` instance that contains an interpolated error
+// string along with a list of causes.
+//
+// The go stdlib does not define output on `Unwrap()` for a multi-cause
+// error, so we default to nil here.
 func UnwrapOnce(err error) (cause error) {
 	switch e := err.(type) {
 	case interface{ Cause() error }:
@@ -39,6 +49,7 @@ func UnwrapOnce(err error) (cause error) {
 
 // UnwrapAll accesses the root cause object of the error.
 // If the error has no cause (leaf error), it is returned directly.
+// UnwrapAll treats multi-errors as leaf nodes.
 func UnwrapAll(err error) error {
 	for {
 		if cause := UnwrapOnce(err); cause != nil {
@@ -48,4 +59,13 @@ func UnwrapAll(err error) error {
 		break
 	}
 	return err
+}
+
+// UnwrapMulti access the slice of causes that an error contains, if it is a
+// multi-error.
+func UnwrapMulti(err error) []error {
+	if me, ok := err.(interface{ Unwrap() []error }); ok {
+		return me.Unwrap()
+	}
+	return nil
 }
